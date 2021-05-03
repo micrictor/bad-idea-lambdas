78a9b979124ab9fce133412a2bdf57cfad8a4da2
Michael Torres <mic.ric.tor@gmail.com>
2021-05-02 18:27:44 -0700

    Set up the plumbing to parse the input

--- lambda-lambda/inceptor/main.go
+++ lambda-lambda/inceptor/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response events.APIGatewayProxyResponse
-type Request events.APIGatewayProxyRequest
+type Response events.APIGatewayV2HTTPResponse
+type Request events.APIGatewayV2HTTPRequest
 
 type Parameters struct {
 	SourceCode string `json:"sourceCode"`
@@ -21,6 +24,18 @@ var HEADERS = map[string]string{
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, request Request) (Response, error) {
+	fmt.Println(request.RequestContext.HTTP.Method, " ", request.RawPath, " ", request.Body)
+	switch request.RequestContext.HTTP.Method {
+	case "POST":
+		return postHandler(request)
+	case "GET":
+		fallthrough
+	default:
+		return getHandler(request)
+	}
+}
+
+func getHandler(request Request) (Response, error) {
 	params := Parameters{}
 	resp := Response{}
 
@@ -41,6 +56,37 @@ func Handler(ctx context.Context, request Request) (Response, error) {
 	}
 
 	return resp, nil
+}
+
+func postHandler(request Request) (Response, error) {
+	params := Parameters{}
+	if request.IsBase64Encoded {
+		if value, err := base64.StdEncoding.DecodeString(request.Body); err != nil {
+			return Response{
+				StatusCode: 400,
+				Body:       fmt.Sprintf("Error parsing base64-encoded parameters: %s", err.Error()),
+				Headers:    HEADERS,
+			}, nil
+		} else {
+			request.Body = string(value)
+		}
+	}
+	fmt.Println("JSON ", request.IsBase64Encoded, " ", request.Body)
+	buf := []byte(request.Body)
+	if err := json.Unmarshal(buf, &params); err != nil {
+		return Response{
+			StatusCode: 400,
+			Body:       "Error parsing JSON body!",
+			Headers:    HEADERS,
+		}, nil
+	} else {
+
+		return Response{
+			StatusCode: 200,
+			Body:       params.SourceCode,
+			Headers:    HEADERS,
+		}, nil
+	}
 }
 
 func main() {
